0ec78ab3e973a571bf47e10cdbc43bf6019ba5f7
phuoc12341 <34267560+phuoc12341@users.noreply.github.com>
2019-04-12 16:24:51 +0700

    Update StubTest.php

--- blog/tests/Unit/StubTest.php
+++ blog/tests/Unit/StubTest.php
@@ -17,15 +17,11 @@ class StubTest extends TestCase
      */
     public function testUnitTestExample()
     {
-        // Create a stub for the SomeClass class.
         $stub = $this->createMock(TestController::class);
 
-        // Configure the stub.
         $stub->method('showName')
              ->willReturn('OK');
 
-        // Calling $stub->doSomething() will now return
-        // 'foo'.
         $this->assertSame('OK', $stub->showName());
     }
 

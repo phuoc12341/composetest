f3ed1d0fb72338e32b2e2e5de498727b8f51bdd0
root <root@b121647-pc.framgia.com>
2019-04-23 15:03:10 +0700

    create_UT_for_ServiceProvider

---
+++ blog/app/Repository/CommentRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Repository;
+
+use Illuminate\Database\Connection;
+use Illuminate\Support\Collection;
+use \Carbon\Carbon;
+
+class CommentRepository implements CommentRepositoryInterface
+{
+    //
+}
